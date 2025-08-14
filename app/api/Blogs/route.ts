57dd12b3dec9b64573ae546ed0015badebd8f876
Ishan Saini <ishansaini0105@gmail.com>
2025-08-14 21:32:32 +0530

    The blog route is done

--- app/api/Blogs/route.ts
+++ app/api/Blogs/route.ts
@@ -73,3 +73,212 @@ export async function POST(req: NextRequest) {
     }
   }
 }
+
+export async function PUT(req: NextRequest) {
+  try {
+    const data = await req.json();
+    const parsedBody = Blogschema.safeParse(data);
+
+    if (!parsedBody.success) {
+      return NextResponse.json(
+        { success: false, error: parsedBody.error.format() },
+        { status: 400 }
+      );
+    }
+
+    const { title, content } = parsedBody.data;
+
+    const token = await getToken({ req, secret: process.env.NEXT_AUTH_SECRET });
+
+    if (!token) {
+      return NextResponse.json(
+        { success: false, error: "token not found" },
+        { status: 401 }
+      );
+    }
+
+    const existUser = await prisma.user.findFirst({
+      where: {
+        id: token.id,
+      },
+    });
+
+    if (!existUser) {
+      return NextResponse.json(
+        { success: false, error: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    const existBlog = await prisma.blog.findFirst({
+      where: {
+        title: title,
+        content: content,
+        authorId: token.id,
+      },
+    });
+
+    if (!existBlog) {
+      return NextResponse.json(
+        { success: false, error: "Blog not found" },
+        { status: 409 }
+      );
+    }
+
+    await prisma.blog.update({
+      where: {
+        id: existBlog.id,
+      },
+      data: {
+        title: title,
+        content: content,
+        authorId: token.id,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Blog updated successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const token = await getToken({ req, secret: process.env.NEXT_AUTH_SECRET });
+    const data = await req.json();
+    const blogId = data.blogId;
+
+    if (!token) {
+      return NextResponse.json(
+        { success: false, error: "token not found" },
+        { status: 400 }
+      );
+    }
+
+    const existUser = await prisma.user.findFirst({
+      where: {
+        id: token.id,
+      },
+    });
+
+    if (!existUser) {
+      return NextResponse.json(
+        { success: false, error: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    const existBlog = await prisma.blog.findFirst({
+      where: {
+        id: blogId,
+      },
+    });
+
+    if (!existBlog) {
+      return NextResponse.json(
+        { success: false, error: "Blog not found" },
+        { status: 409 }
+      );
+    }
+
+    await prisma.blog.delete({
+      where: {
+        id: blogId,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Blog deleted successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = await getToken({ req, secret: process.env.NEXT_AUTH_SECRET });
+
+    if (!token) {
+      return NextResponse.json(
+        { success: false, error: "token not found" },
+        { status: 400 }
+      );
+    }
+
+    const existUser = await prisma.user.findFirst({
+      where: {
+        id: token.id,
+      },
+    });
+
+    if (!existUser) {
+      return NextResponse.json(
+        { success: false, error: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    const blogs = await prisma.blog.findMany({
+      where: {
+        authorId: token.id,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        blogs,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
