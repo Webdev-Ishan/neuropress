39e6eeb78c456038db39729bb9c157682c771c89
Ishan Saini <ishansaini0105@gmail.com>
2025-08-14 18:44:13 +0530

    The auth backend is ready

---
+++ app/api/auth/Profile/route.ts
@@ -0,0 +1,53 @@
+import { prisma } from "@/lib/DB";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = await getToken({ req, secret: process.env.NEXT_AUTH_SECRET });
+    if (!token) {
+      return NextResponse.json(
+        { success: false, error: "Login Please" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { id: token.id },
+      select: {
+        blogs: {
+          select: {
+            title: true,
+            content: true,
+          },
+        },
+      },
+    });
+
+    if (!existingUser) {
+      return NextResponse.json(
+        { success: false, error: "User does not exist, Please Register" },
+        { status: 402 }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, existingUser },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
