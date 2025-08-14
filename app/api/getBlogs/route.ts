6a9a4f1cdd3e3f88489ab0ace1668a2d22989dcb
Ishan Saini <ishansaini0105@gmail.com>
2025-08-14 21:45:08 +0530

    The get blog by id route is done

---
+++ app/api/getBlogs/route.ts
@@ -0,0 +1,60 @@
+import { prisma } from "@/lib/DB";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = await getToken({ req, secret: process.env.NEXT_AUTH_SECRET });
+    const data = await req.json();
+    const blogId = data.blogId;
+
+    if (!token || !blogId) {
+      return NextResponse.json(
+        { success: false, error: "token or blogID not found" },
+        { status: 400 }
+      );
+    }
+
+    const existUser = await prisma.user.findFirst({
+      where: {
+        id: token.id,
+      },
+    });
+
+    if (!existUser) {
+      return NextResponse.json(
+        { success: false, error: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    const blog = await prisma.blog.findUnique({
+      where: {
+        id: token.id,
+        authorId: token.id,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        blog,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
