8b1ecf7a4fb23faef986da354943dc978ecbea74
Ishan Saini <ishansaini0105@gmail.com>
2025-08-14 21:41:00 +0530

    The update profile route is done

--- app/api/auth/Profile/route.ts
+++ app/api/auth/Profile/route.ts
@@ -1,6 +1,14 @@
 import { prisma } from "@/lib/DB";
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+import bcrypt from "bcrypt";
+
+const registerSchema = z.object({
+  username: z.string().min(3).max(12),
+  email: z.email(),
+  password: z.string().min(6).max(12),
+});
 
 export async function GET(req: NextRequest) {
   try {
@@ -34,6 +42,77 @@ export async function GET(req: NextRequest) {
 
     return NextResponse.json(
       { success: true, existingUser },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    const data = await req.json();
+    const parsedBody = registerSchema.safeParse(data);
+
+    if (!parsedBody.success) {
+      return NextResponse.json(
+        { success: false, error: parsedBody.error.format() },
+        { status: 400 }
+      );
+    }
+
+    const { username, email, password } = parsedBody.data;
+
+    const existingUser = await prisma.user.findFirst({
+      where: { email: email },
+    });
+
+    if (!existingUser) {
+      return NextResponse.json(
+        { success: false, error: "User does not exist" },
+        { status: 402 }
+      );
+    }
+
+    const decodepassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    const saltvalue = await bcrypt.genSalt(Number(10));
+    const hashedPassword = await bcrypt.hash(password, saltvalue);
+
+    if (
+      existingUser.username !== username ||
+      existingUser.email !== email ||
+      !decodepassword
+    ) {
+      await prisma.user.update({
+        where: {
+          email: email,
+        },
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+    }
+
+    return NextResponse.json(
+      { success: true, message: "Updation Successfull" },
       {
         status: 200,
       }
